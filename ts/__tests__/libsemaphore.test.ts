944eda6be3f35173c3f465f78fa658e08ee16196
Koh Wei Jie <contact@kohweijie.com>
2019-09-17 22:05:25 +0800

    added simple tests for Identity and identity commitment generation

--- ts/__tests__/libsemaphore.test.ts
+++ ts/__tests__/libsemaphore.test.ts
@@ -5,11 +5,24 @@ const path = require('path')
 const uuidv4 = require('uuid/v4')
 
 const assert = chai.assert
-//const expect = chai.expect;
+
+import * as libsemaphore from '../index'
 
 describe('libsemaphore', function () {
+    const identity = libsemaphore.genIdentity()
 
-    it('EdDSA functions', async () => {
-        console.log('hi')
+    it('genIdentity() should produce values of the correct length and type', async () => {
+        assert.equal(identity.keypair.pubKey.length, 2)
+        assert.equal(identity.keypair.privKey.length, 32)
+        assert.equal(typeof identity.identityNullifier, 'bigint')
+        assert.equal(typeof identity.identityTrapdoor, 'bigint')
+    })
+
+    it('identityCommitment() should produce a value of the correct length and type', async () => {
+        const idc = libsemaphore.genIdentityCommitment(identity)
+        assert.equal(typeof idc, 'bigint')
+        assert.isBelow(idc.toString(16).length, 65)
+        // This may fail in very rare occasions; just run the test again
+        assert.isAbove(idc.toString(16).length, 48)
     })
 })
--- ts/index.ts
+++ ts/index.ts
@@ -278,4 +278,6 @@ export {
     genPublicSignals,
     verifyProof,
     verifySignature,
+    signMsg,
+    genIdentityCommitment,
 }
